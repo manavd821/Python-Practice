5812e3f02c9d55a6f65c365a8cc0a4ff53f9d967
manavok <mitmanav6805@gmail.com>
2025-05-20 14:38:29 +0530

    read only attribute or property decorator

--- OOPs/polymorphism.py
+++ OOPs/polymorphism.py
@@ -3,20 +3,24 @@ class Car:
     total_car = 0;
 
     def __init__(self, brand, model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
         Car.total_car +=1 
         # self.total_car +=1
 
     def full_name(self):
-        return f"{self.brand}, {self.model}"
+        return f"{self.__brand}, {self.model}"
     
     def fuel_type(self):
         return "Petrol or Diseal"
+    
     # static method
     @staticmethod
     def general_description():
         return "It is car bhai"
+    @property
+    def brand(self):
+        return self.__brand
 
 # inheritance
 class ElectricCar(Car):
@@ -43,3 +47,7 @@ print(Car.total_car) # it include child class too
 # print(my_car.general_description()) object can't excess static method
 print(Car.general_description())
 print(ElectricCar.general_description()) # child can also access the static method of parent
+
+new_car = Car("safari", "ABC")
+# new_car.brand = "city"
+print(new_car.brand)
