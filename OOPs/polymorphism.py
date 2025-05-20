202edf6c785c2f4676ee9895ce566d5ff1059754
manavok <mitmanav6805@gmail.com>
2025-05-20 14:22:55 +0530

    learn polymorphism and total_number_of_object

---
+++ OOPs/polymorphism.py
@@ -0,0 +1,37 @@
+# basic
+class Car:
+    total_car = 0;
+
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        Car.total_car +=1 
+        # self.total_car +=1
+
+    def full_name(self):
+        return f"{self.brand}, {self.model}"
+    def fuel_type(self):
+        return "Petrol or Diseal"
+
+# inheritance
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric Charge"
+
+
+# basic
+my_car = Car("Toyato", "Corolla")
+
+# inheritance and polymorphism
+my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+print(my_car.fuel_type())
+print(my_tesla.fuel_type())
+#  calculate number of object
+test = Car("ok","ok")
+print(Car.total_car) # it include child class too
+# print(test.total_car)
+# print(my_car.total_car)
