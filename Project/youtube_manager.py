cd1cf45e846dfe68d53f4559447e158c6cba927a
manavok <mitmanav6805@gmail.com>
2025-05-27 19:10:36 +0530

    Complete the Project

--- Project/youtube_manager.py
+++ Project/youtube_manager.py
@@ -1,14 +1,94 @@
+import json
+ytFile = 'youtube.txt'
 def load_data():
-    pass
+    try:
+        with open(ytFile, 'r') as file:
+            test = json.load(file)
+            # print(test)
+            return test
+    except FileNotFoundError:
+            return []
+
+def add_data_to_file(videos):
+    with open(ytFile, 'w') as file:
+        json.dump(videos,file)
 
 def list_all_videos(videos):
-    pass
+    print('*' * 70)
+    for idx, video in enumerate(videos, start = 1):
+        print(f"{idx}. {video['name']}, duration: {video['time']}")
+
 def add_video(videos):
-    pass
+    name = input("Enter video name: ")
+    time = input("Enter duration: ")
+    videos.append({'name':name, 'time':time})
+    add_data_to_file(videos)
+
 def update_video(videos):
-    pass
+    try:
+        change = int(input("Which video do you want to change (index): "))
+        if change < 1 and change > len(videos):
+            print("Invalid index!")
+            return
+    except ValueError:
+        print("Invalid index!")
+        return
+    
+    choice = input("What do you want to change:\n1).name 2).duration 3).both\nchoose index: ")
+    newName = None
+    newTime = None
+
+    if choice == '1':
+        newName = input("Enter new name: ")
+    elif choice == '2':
+        newTime = input("Enter new duration: ")
+    elif choice == '3':
+        newName = input("Enter new name: ")
+        newTime = input("Enter new duration: ")
+    else:
+        print("Enter correct option (1, 2, or 3)!")
+        return
+    
+    # for idx, video in enumerate(videos, start=1):
+    #     if idx == change:
+    #         if newName:
+    #             video['name'] = newName
+    #         if newTime:
+    #             video['time'] = newTime
+    #         add_data_to_file(videos)
+    #         return
+    # print("Enter correct value:")
+
+    # or methode: 2
+    video = videos[change-1]
+    if newName:
+        video['name'] = newName
+    if newTime:
+        video['time'] = newTime
+
+    add_data_to_file(videos)
+    print("Video updated successfully.")
+
+
 def delete_video(videos):
-    pass
+    try:
+        change = int(input("Which video do you want to delete (index): "))
+        if change < 1 and change > len(videos):
+            print("Invalid index!")
+            return
+    except ValueError:
+        print("Invalid index!")
+        return
+    # removed = videos.pop(change-1)
+    # print(removed) # it will give you deleted video
+    # add_data_to_file(videos)
+    # print("Video deleted successfully.")
+
+    # methode 2:
+    del videos[change-1]
+    add_data_to_file(videos)
+    print("Video deleted successfully.")
+
 
 def main():
     videos = load_data()
